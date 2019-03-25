91846bb48c73711f8fc52009a4e2ad9f3687891f
yl <yangli@itcast.cn>
2019-03-25 22:10:16 +0800

    家的境况

--- demo-test/src/test/java/test1.java
+++ demo-test/src/test/java/test1.java
@@ -6,15 +6,10 @@ public class test1 {
     public void test2(){
             System.out.println("11111111");
             System.out.println("22222222");
-<<<<<<< HEAD
-
 		System.out.println("3333333333");		
 		System.out.println("4444444444");		
 		System.out.println("3333333333");
 		System.out.println("5555555555");
-				
-
-=======
 		System.out.println("3333333333");
 		System.out.println("5555555555");
 				
