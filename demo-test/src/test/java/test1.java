b3f0bc2ac064debb4eec2168f125bd5d8664a543
yl <yangli@itcast.cn>
2019-03-25 21:53:10 +0800

    5555555555

--- demo-test/src/test/java/test1.java
+++ demo-test/src/test/java/test1.java
@@ -6,6 +6,8 @@ public class test1 {
     public void test2(){
             System.out.println("11111111");
             System.out.println("22222222");
-		System.out.println("3333333333");		
+		System.out.println("3333333333");
+		System.out.println("5555555555");
+				
     }
 }
