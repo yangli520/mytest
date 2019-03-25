2e59e04e3d5477e05398b939f4603a202f1a51dd
yl <yangli@itcast.cn>
2019-03-25 22:29:00 +0800

    213465

--- demo-test/src/test/java/test1.java
+++ demo-test/src/test/java/test1.java
@@ -18,17 +18,14 @@ public class test1 {
 
             System.out.println("11111111");
             System.out.println("22222222");
-<<<<<<< HEAD
 		System.out.println("3333333333");		
 		System.out.println("4444444444");		
 		System.out.println("3333333333");
 		System.out.println("5555555555");
 		System.out.println("3333333333");
 		System.out.println("5555555555");
-=======
 		System.out.println("3333333333");
 		System.out.println("5555555555");
 				
->>>>>>> dev
     }
 }
