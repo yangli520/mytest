714fb58575033b646d861aa65e663832320e4944
yl <yangli@itcast.cn>
2019-03-25 22:36:49 +0800

    2524242

--- demo-test/src/test/java/test1.java
+++ demo-test/src/test/java/test1.java
@@ -5,27 +5,6 @@ public class test1 {
     @Test
     public void test2(){
 
-
-
-
-
-
-
-
-
-				
-
-
-            System.out.println("11111111");
-            System.out.println("22222222");
-		System.out.println("3333333333");		
-		System.out.println("4444444444");		
-		System.out.println("3333333333");
-		System.out.println("5555555555");
-		System.out.println("3333333333");
-		System.out.println("5555555555");
-		System.out.println("3333333333");
-		System.out.println("5555555555");
 				
     }
 }
